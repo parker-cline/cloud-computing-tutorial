516a1caa1ccb6d6cafd7d34b31c744d1ed10231e
Parker Cline <parkerpawleysisland@gmail.com>
2022-12-16 20:37:05 -0300

    add nat gateway and eip

--- vpc.tf
+++ vpc.tf
@@ -258,3 +258,16 @@ resource "aws_security_group" "db_instance" {
 
   vpc_id = aws_vpc.main.id
 }
+
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id = aws_subnet.public-subnet.id
+  tags = {
+    "Name" = "DummyNatGateway"
+  }
+  depends_on = [aws_internet_gateway.gw]
+}
